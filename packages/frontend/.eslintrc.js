1bf72648b9b16bd57bbe1a07c1f31cef47152bd5
Takuya Eguchi <seal2501@gmail.com>
2022-10-01 11:24:09 +0900

    chore: install storybook (`npx sb init` in `frontend`)

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@portfolio/eslint-config', 'next/core-web-vitals'],
+  extends: ['@portfolio/eslint-config', 'next/core-web-vitals', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint']
 };
